57fd5bb992d8c614c6fc93f4a114ff25e5c72720
kowsalyajaganathan <kowsalyajaganathan@gmail.com>
2016-03-22 09:42:05 -0600

    Added files via upload

---
+++ src/com/ers/util/Constants.java
@@ -0,0 +1,65 @@
+package com.ers.util;
+
+
+/**
+ *Constants.java
+ *
+ *Version:1.0
+ *Date: 21-Mar-2016
+ *Author:Kowsalya Jaganathan
+ *
+ *This class is to hold the constant values to be used through out the application
+ * 
+ *
+ *
+*/
+
+public class Constants{
+	public static final String JDBC_DRIVER="";
+	public static final String DB_URL="";
+	//public static final String JDBC_DRIVER="";
+	//public static final String JDBC_DRIVER="";
+	//public static final String JDBC_DRIVER="";
+	
+	public static final String FORM_CERTIFICATION="certification";
+	public static final String FORM_TEAMEVENT="teamevent";
+	public static final String FORM_TRAINING="training";
+	public static final String FORM_TRAVEL="travel";
+	
+	
+	
+	public static final String CERTIFICATION_CREATE="certification_create";
+	public static final String TEAMEVENT_CREATE="teamevent_create";
+	public static final String TRAINING_CREATE="training_create";
+	public static final String TRAVEL_CREATE="travel_create";
+	public static final String CERTIFICATION_DELETE="certification_delete";
+	public static final String TEAMEVENT_DELETE="teamevent_delete";
+	public static final String TRAINING_DELETE="training_delete";
+	public static final String TRAVEL_DELETE="travel_delete";
+	public static final String CERTIFICATION_UPDATE="certification_update";
+	public static final String TEAMEVENT_UPDATE="teamevent_update";
+	public static final String TRAINING_UPDATE="training_update";
+	public static final String TRAVEL_UPDATE="travel_update";
+	public static final String CERTIFICATION_APPROVE="certification_approve";
+	public static final String TEAMEVENT_APPROVE="teamevent_approve";
+	public static final String TRAINING_APPROVE="training_approve";
+	public static final String TRAVEL_APPROVE="travel_approve";
+	public static final String CERTIFICATION_REJECT="certification_reject";
+	public static final String TEAMEVENT_REJECT="teamevent_reject";
+	public static final String TRAINING_REJECT="training_reject";
+	public static final String TRAVEL_REJECT="travel_reject";
+	
+	
+	
+	public static final String ACTION_CREATE="create";
+	public static final String ACTION_DELETE="delete";
+	public static final String ACTION_UPDATE="update";
+	public static final String ACTION_APPROVE="approve";
+	public static final String ACTION_REJECT="reject";
+
+	public static final String STATUS_CREATED="submitted";
+	//public static final String STATUS_DELETED="delete";
+	public static final String STATUS_UPDATED="updated";
+	public static final String STATUS_APPROVED="approved";
+	public static final String STATUS_REJECTED="rejected";
+}
